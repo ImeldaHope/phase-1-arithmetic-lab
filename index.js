4f12c01420ae366d26b6c32ada5dee5f8456acf4
Imelda Hope <imeldalarusso@gmail.com>
2024-06-24 22:06:18 +0300

    Performs arithmetic operations

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+//mulitply as an equation
+
+const num1 = 2;
+const num2 = 31
+const multiply = num1 * num2;
+multiply;
+
+const random = Math.floor(Math.random() * 1000)+1;
+random;
+
+const num3 = 19;
+const num4 = 15;
+const mod = num3 % num4;
+mod;
+
+const max = Math.max(3,4,7,8,11,13,15,16,17,19,20);
+max;
